1026b5c1fd9808998aeb4e824615f40a0653646a
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-20 16:34:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 def game_hash
   {
@@ -126,4 +127,97 @@ def game_hash
   }
 end
 
-# Write code here
+#num_points_scored returns points scored for each player
+#takes in an argument of a player's name and
+#returns the number of points scored for that player.
+def get_players
+  players = game_hash.keys.map do |team|
+    game_hash[team][:players]
+  end
+  players.flatten
+end
+
+def num_points_scored(player_name)
+  #Iterate through top level
+  #game_hash.each do |location, team_data| #home and away teams
+  #get all players
+    found_player = get_players.find do |player|
+      player[:player_name] == player_name
+    end
+  found_player[:points]
+end
+
+
+def shoe_size(player_name)
+#takes in an argument of a player's name and returns the shoe size for that player
+  found_player = get_players.find do |player|
+    #if player's name is equal to player_name
+    player[:player_name] == player_name
+  end
+  #return the shoe size
+  found_player[:shoe]
+end
+
+
+
+def team_names
+  #operates on the game Hash to return an Array of the team names.
+  game_hash.map do |team, team_info|
+    team_info[:team_name]
+  end
+
+end
+
+def team_colors(team_input)
+#takes in an argument of the team name and returns an Array of that team's colors.
+  if team_input.downcase == "charlotte hornets"
+    return game_hash[:away][:colors]
+  else return game_hash[:home][:colors]
+  end
+end
+
+def player_numbers(input)
+#takes in an argument of a team name and returns an Array of the jersey numbers for that team.
+  output = []
+  game_hash.each do |team, team_info|
+    if team_info[:team_name] == input
+      team_info.each do |key, value|
+        if key == :players
+          value.each do |player|
+            output.push(player[:number])
+          end
+        end
+      end
+    end
+    return output
+end
+end
+
+def player_stats(input)
+  game_hash.each do |team, team_info|
+    team_info.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if input == player[:player_name]
+            player.delete(:player_name)
+            return player
+          end
+        end
+      end
+    end
+  end
+end
+
+def big_shoe_rebounds
+  big_shoe = 0
+  rebounds = 0
+  game_hash.each do |team, team_info|
+    team_info[:players].each do |player|
+      if player[:shoe] > big_shoe
+        big_shoe = player[:shoe]
+        rebounds = player[:rebounds]
+      end
+    end
+  end
+  return rebounds
+end
