507101a985c726dc7e7057935d5d50f92c22ba6f
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-20 17:11:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -178,22 +178,24 @@ end
 
 def player_numbers(team_name)
 #takes in an argument of a team name and returns an Array of the jersey numbers for that team.
-  output = []
-  game_hash.collect do |team, stats|
-    if stats[:team_name] ==team_name
-      stats[:players].collect do |player, data|
-        output.push(data[:number])
+  #output = []
+  game_hash.map do |team, team_info|
+    team_info[:number]
+    #if stats[:team_name] ==team_name
+    #  stats[:players].collect do |player, data|
+    #    output.push(data[:number])
   #game_hash.each do |team, team_info|
   #  if team_info[:team_name] == team_name
   #    team_info.each do |key, value|
   #      if key == :players
   #        value.each do |player|
   #          output.push(player[:number])
-          end
-        end
-      end
-    return output
+        #  end
+        #end
+      #end
+    #return output
   end
+end
 
 def player_stats(player_name)
 #returns all stats for a given player
